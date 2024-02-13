94d12e627dc380291c47bf2a6509109e484c6226
Anish7-anish <tanishraosams7@gmail.com>
2024-02-13 17:07:28 -0500

    BugFix

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,25 +1,17 @@
 class RoomsController < ApplicationController
-  before_action :set_room, only: %i[ show edit update destroy ]
+  before_action :set_room, only: [:show, :edit, :update, :destroy]
 
   # GET /rooms or /rooms.json
   def index
     @rooms = Room.all
 
-    if params[:desired_start_time].present? && params[:desired_end_time].present?
-      desired_start_time = DateTime.parse(params[:desired_start_time])
-      desired_end_time = DateTime.parse(params[:desired_end_time])
+    if params[:date].present? && params[:start_time].present? && params[:end_time].present?
+      # Retrieve available rooms based on the desired time slot
+      @available_rooms = Room.available_rooms(params[:date], params[:start_time], params[:end_time])
     else
-      # Default values if desired start and end times are not provided
-      desired_start_time = Time.now
-      desired_end_time = Time.now + 1.hour
+      # If date and time parameters are not provided, show all rooms
+      @available_rooms = @rooms
     end
-
-    # Pass the variables to the view
-    @desired_start_time = desired_start_time
-    @desired_end_time = desired_end_time
-
-    # Retrieve available rooms based on the desired time slot
-    @available_rooms = Room.available_rooms(desired_start_time, desired_end_time)
   end
 
   # GET /rooms/1 or /rooms/1.json
@@ -41,10 +33,10 @@ class RoomsController < ApplicationController
 
     respond_to do |format|
       if @room.save
-        format.html { redirect_to room_url(@room), notice: "Room was successfully created." }
+        format.html { redirect_to @room, notice: 'Room was successfully created.' }
         format.json { render :show, status: :created, location: @room }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new }
         format.json { render json: @room.errors, status: :unprocessable_entity }
       end
     end
@@ -54,10 +46,10 @@ class RoomsController < ApplicationController
   def update
     respond_to do |format|
       if @room.update(room_params)
-        format.html { redirect_to room_url(@room), notice: "Room was successfully updated." }
+        format.html { redirect_to @room, notice: 'Room was successfully updated.' }
         format.json { render :show, status: :ok, location: @room }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit }
         format.json { render json: @room.errors, status: :unprocessable_entity }
       end
     end
@@ -65,10 +57,9 @@ class RoomsController < ApplicationController
 
   # DELETE /rooms/1 or /rooms/1.json
   def destroy
-    @room.destroy!
-
+    @room.destroy
     respond_to do |format|
-      format.html { redirect_to rooms_url, notice: "Room was successfully destroyed." }
+      format.html { redirect_to rooms_url, notice: 'Room was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,9 +1,13 @@
 class Room < ApplicationRecord
   has_many :events
 
-  def self.available_rooms(start_time, end_time)
+  def self.available_rooms(date, start_time, end_time)
+    # Convert date, start_time, and end_time to DateTime objects
+    start_datetime = DateTime.parse("#{date} #{start_time}")
+    end_datetime = DateTime.parse("#{date} #{end_time}")
+
     # Get all rooms that are not booked during the specified time slot
-    booked_room_ids = Event.where("start_time < ? AND end_time > ?", end_time, start_time).pluck(:room_id)
+    booked_room_ids = Event.where("start_time < ? AND end_time > ?", end_datetime, start_datetime).pluck(:room_id)
     where.not(id: booked_room_ids)
   end
 end
