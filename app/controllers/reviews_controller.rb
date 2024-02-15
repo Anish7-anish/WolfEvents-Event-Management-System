9d1503dd86f8c380bfa968cfd783f34ceb8a9c5a
Mahathi <mahathi252001@gmail.com>
2024-02-15 17:40:15 -0500

    Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,26 +1,43 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: %i[ show edit update destroy ]
   before_action :check_event_category, only: [:new], unless: :admin?
+  before_action :set_reviews, only: [:index]
+
 
   # GET /reviews or /reviews.json
   def index
-    @reviews = Review.all
+    # For admin users, fetch all reviews
+    # For regular users, fetch only their own reviews
+    @reviews = current_user.is_admin ? Review.all : current_user.reviews
 
+    # Apply additional filters based on parameters passed in the request
+    @reviews = filter_reviews(@reviews, params)
+  end
+
+  private
+
+  def set_reviews
+    # No need to set reviews separately now, as it's done within the index action
+  end
+
+  def filter_reviews(reviews, params)
     if params[:attendee_name].present?
-      @reviews = @reviews.joins(:attendee).where("attendees.name LIKE ?", "%#{params[:attendee_name]}%")
+      reviews = reviews.joins(:attendee).where("attendees.name LIKE ?", "%#{params[:attendee_name]}%")
     end
 
     if params[:event_id].present?
-      @reviews = @reviews.joins(:event).where(events: { id: params[:event_id] })
+      reviews = reviews.joins(:event).where(events: { id: params[:event_id] })
     end
 
     if params[:attendee_email].present?
-      @reviews = @reviews.joins(:attendee).where("attendees.email LIKE ?", "%#{params[:attendee_email]}%")
+      reviews = reviews.joins(:attendee).where("attendees.email LIKE ?", "%#{params[:attendee_email]}%")
     end
 
     if params[:event_name].present?
-      @reviews = @reviews.joins(:event).where("events.name LIKE ?", "%#{params[:event_name]}%")
+      reviews = reviews.joins(:event).where("events.name LIKE ?", "%#{params[:event_name]}%")
     end
+
+    reviews
   end
 
   # GET /reviews/1 or /reviews/1.json
@@ -49,67 +66,80 @@ class ReviewsController < ApplicationController
       end
       if event.date >= Date.current
         redirect_to events_path, alert: "Reviews can only be written for events that have already occurred."
-        return
+
+        unless admin?
+          event = Event.find_by(id: @review.event_id)
+          if event.nil?
+            redirect_to root_path, alert: "Event not found." and return
+          elsif event.category.in?(['Miscellaneous', 'Private'])
+            redirect_to root_path, alert: "Reviews are not allowed for events in Miscellaneous/Private categories." and return
+          end
+        end
+
+        respond_to do |format|
+          if @review.save
+            format.html { redirect_to review_url(@review), notice: "Review was successfully created." }
+            format.json { render :show, status: :created, location: @review }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @review.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+
+
+
+      # PATCH/PUT /reviews/1 or /reviews/1.json
+      def update
+        respond_to do |format|
+          if @review.update(review_params)
+            format.html { redirect_to review_url(@review), notice: "Review was successfully updated." }
+            format.json { render :show, status: :ok, location: @review }
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+            format.json { render json: @review.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+      # DELETE /reviews/1 or /reviews/1.json
+      def destroy
+        @review.destroy!
+
+        respond_to do |format|
+          format.html { redirect_to reviews_url, notice: "Review was successfully destroyed." }
+          format.json { head :no_content }
+        end
+      end
+
+      private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_review
+        @review = Review.find(params[:id])
+      end
+
+      # Only allow a list of trusted parameters through.
+      def review_params
+        params.require(:review).permit(:attendee_id, :event_id, :rating, :feedback)
+      end
+
+      def admin?
+        current_user&.is_admin?
+      end
+
+
+      def check_event_category
+        event = Event.find(params[:event_id])
+        if event.category.in?(['Miscellaneous', 'Private'])
+          redirect_to root_path, alert: "Reviews are not allowed for events in Miscellaneous/Private categories."
+        end
+        if event.date >= Date.current
+          redirect_to events_path, alert: "Reviews can only be written for events that have already occurred."
+        end
       end
     end
-    respond_to do |format|
-      if @review.save
-        format.html { redirect_to review_url(@review), notice: "Review was successfully created." }
-        format.json { render :show, status: :created, location: @review }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
-    end
+
   end
 
-  # PATCH/PUT /reviews/1 or /reviews/1.json
-  def update
-    respond_to do |format|
-      if @review.update(review_params)
-        format.html { redirect_to review_url(@review), notice: "Review was successfully updated." }
-        format.json { render :show, status: :ok, location: @review }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /reviews/1 or /reviews/1.json
-  def destroy
-    @review.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to reviews_url, notice: "Review was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_review
-    @review = Review.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def review_params
-    params.require(:review).permit(:attendee_id, :event_id, :rating, :feedback)
-  end
-
-  def admin?
-    current_user&.is_admin?
-  end
-
-
-  def check_event_category
-    event = Event.find(params[:event_id])
-    if event.category.in?(['Miscellaneous', 'Private'])
-      redirect_to root_path, alert: "Reviews are not allowed for events in Miscellaneous/Private categories."
-    end
-    if event.date >= Date.current
-      redirect_to events_path, alert: "Reviews can only be written for events that have already occurred."
-      return
-    end
-  end
 end
