494c83a316966539d67c74a85b1d04db53dd2f75
kgudipe <kgudipe@ncsu.edu>
2024-02-16 14:19:54 -0500

    Overlapping events creation done

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   belongs_to :room
   has_many :event_tickets, dependent: :destroy
   has_many :reviews, dependent: :destroy
-  validate :check_room_availability
+  validate :no_overlapping_events
   validate :start_time_must_be_in_future
 
   def start_time_must_be_in_future
@@ -22,23 +22,7 @@ class Event < ApplicationRecord
 
 
 
-  def check_room_availability
-    if room_id && overlaps_with_other_events?
-      errors.add(:base, "The selected room is not available during this time slot")
-    end
-  end
 
-
-
-  def overlaps_with_other_events?
-    if persisted? # Exclude self from the query when updating an existing record
-      overlapping_events = room.events.where.not(id: id).where("start_time < ? AND end_time > ?", end_time, start_time)
-    else
-      overlapping_events = room.events.where("start_time < ? AND end_time > ?", end_time, start_time)
-    end
-
-    overlapping_events.exists?
-  end
 
   def decrease_seats_left
     self.number_of_seats_left -= 1
@@ -52,5 +36,25 @@ class Event < ApplicationRecord
 
   scope :upcoming, -> { where('date >= ?', Date.today) }
   scope :not_sold_out, -> { where('number_of_seats_left > 0') }
+
+
+  def no_overlapping_events
+    return unless room && date && start_time && end_time
+
+    # Convert 'date' and 'time' attributes to full DateTime objects for the event being validated
+    event_start_datetime = DateTime.parse("#{date} #{start_time.strftime('%H:%M:%S')}")
+    event_end_datetime = DateTime.parse("#{date} #{end_time.strftime('%H:%M:%S')}")
+
+    overlapping_events = room.events.where.not(id: id).select do |e|
+      # Convert each event's 'date' and 'time' attributes to full DateTime objects
+      e_start_datetime = DateTime.parse("#{e.date} #{e.start_time.strftime('%H:%M:%S')}")
+      e_end_datetime = DateTime.parse("#{e.date} #{e.end_time.strftime('%H:%M:%S')}")
+
+      # Check if the current event overlaps with existing events
+      (event_start_datetime...event_end_datetime).overlaps?(e_start_datetime...e_end_datetime)
+    end
+
+    errors.add(:base, "There is already an event scheduled in this room during the selected time slot.") if overlapping_events.any?
+  end
 
 end
