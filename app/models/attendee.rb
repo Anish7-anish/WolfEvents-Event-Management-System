82eb782bc2629612506a1bd81dac10c61f923276
Anish7-anish <tanishraosams7@gmail.com>
2024-02-21 19:18:26 -0500

    BugFix

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -6,6 +6,6 @@ class Attendee < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates :email, presence: true, format: { with: /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}\z/i }
-  validates :phone_number, presence: true, length: { is: 10 }, numericality: { only_integer: true }
-  validates :credit_card_info, presence: true, length: { is: 12 }, numericality: { only_integer: true }
+  validates :phone_number, presence: true, format: { with: /\A\+1\d{10}\z/, message: "must be in the format +1 followed by 10 digits" }
+  validates :credit_card_info, presence: true, length: { is: 16 }, numericality: { only_integer: true }
 end
